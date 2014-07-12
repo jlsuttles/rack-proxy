e26fbced5aa036e5410c588b00b40ac2a03e8dee
Jessica Lynn Suttles <jlsuttles@gmail.com>
2014-07-12 16:01:39 -0700

    Hardcode content length

--- lib/rack/proxy.rb
+++ lib/rack/proxy.rb
@@ -80,6 +80,7 @@ module Rack
       # Setup body
       if target_request.request_body_permitted? && source_request.body
         target_request.body_stream    = source_request.body
+        target_request.content_length = 100
         target_request.content_type   = source_request.content_type if source_request.content_type
         target_request.body_stream.rewind
       end
